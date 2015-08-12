d3ffc11d3381144c0d63c9a1061f22c7cd8616b4
danrsles <danrsles@gmail.com>
2015-08-12 05:32:59 -0700

    latest ICON version with yc
    includes yc, with double binding(type ahead or using the checkboxes)
    and proper payload has been created

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -14,7 +14,7 @@ return bar;}};
 var diseaseCol = ['Diphtheria', 'Tetanus', 'Pertussis', 'Poliomyelitis', 'Invasive haemophilus influenzae disease', 'Pneumococcal disease', 'Rotavirus enteritis',
                         'Measles', 'Mumps', 'Rubella', 'Varicella', 'Meningococcal disease', 'Type B viral hepatitis', 'Human papilloma virus infection', 'Influenza', 'Type A viral hepatitis'];
  
-var vaccineNames = ['Diphtheria', 'Tetanus', 'Pertussis', 'Polio', 'Hib', 'Pneomococcal', 'Rotavirus', 'Measles', 'Mumps', 'Rubella', 'Varicella', 'Meningococcal', 'Hepatitis B', 'HPV', 'Influenza', 'Hepatitis A'];
+var vaccineNames = ['Diphtheria', 'Tetanus', 'Pertussis', 'Polio','IPV','OPV', 'Hib', 'Pneomococcal', 'PneumoConjugate','Pneumo Poly','Rotavirus', 'Measles', 'Mumps', 'Rubella', 'Varicella', 'Meningococcal','Men-Conjugate C','Men-Conjugate ACYW', 'Hepatitis B', 'HPV', 'Influenza', 'Hepatitis A'];
  
 var colNames = ['Date', 'Vaccine Brand', 'Diphtheria', 'Tetanus', 'Pertussis', 'Poliomyelitis', 'Invasive haemophilus influenzae disease', 'Pneumococcal disease', 'Rotavirus enteritis',
                         'Measles', 'Mumps', 'Rubella', 'Varicella', 'Meningococcal disease', 'Type B viral hepatitis', 'Human papilloma virus infection', 'Influenza', 'Type A viral hepatitis', 'Other', 'Lot Number', 'Transcribed', 'Provider'];
@@ -29,7 +29,8 @@ $scope.bar.value=v;
 
 app.controller('homeController', function ($scope, $modal,$http, $log,connect) {
 $scope.cities= ['Toronto']
-$scope.dem={done:false, pass:false, health:'', fname:'',mname:'',lname:'',gender:'',day:'',addressType:'',unumber:'',city:'',poCode:'',SubmitterFN:'',SubmitterLN:'',sr:'',SubmitterPhoneNumber:'',sEA:'',STN:'',RPO:'',Ruralroute:''};    
+$scope.sts=["Abbey","Acres","Alley","Allée","Autoroute","Avenue","Avenue","Bay","Beach","Bend","Boulevard","Boulevard","By-pass","Byway","Campus","Cape","Carrefour","Carré","Centre","Centre","Cercle","Chase","Chemin","Circle","Circuit","Close","Common","Concession","Corners","Cour","Cours","Court","Cove","Crescent","Croissant","Crossing","Cul-de-sac","Côte","Dale","Dell","Diversion","Downs","Drive","Driveway","End","Esplanade","Estates","Expressway","Extension","Farm","Field","Forest","Freeway","Front","Gardens","Gate","Glade","Glen","Green","Grounds","Grove","Harbour","Heath","Heights","Highlands","Highway","Hill","Hollow","Impasse","Inlet","Island","Key","Knoll","Landing","Lane","Limits","Line","Link","Lookout","Loop","Mall","Manor","Maze","Meadow","Mews","Montée","Moor","Mount","Mountain","Orchard","Parade","Parc","Park","Parkway","Passage","Path","Pathway","Pines","Place","Place","Plateau","Plaza","Point","Pointe","Port","Private","Promenade","Quai","Quay","Ramp","Rang","Range","Ridge","Rise","Road","Rond-point","Route","Row","Rue","Ruelle","Run","Sentier","Sideroad","Square","Street","Subdivision","Terrace","Terrasse","Thicket","Towers","Townline","Trail","Turnabout","Vale","Via","View","Village","Villas","Vista","Voie","Walk","Way","Wharf","Wood","Wynd","Échangeur","Île"];
+$scope.dem={done:false, pass:false, health:'', fname:'',mname:'',lname:'',gender:'',day:'',addressType:'',unumber:'',stnum:'',streetName:'',city:'',streetType:'',streetDirection:'',poBox:'',prov:'',country:'Canada',poCode:'',SubmitterFN:'',SubmitterLN:'',sr:'',SubmitterPhoneNumber:'',SubmitterPhoneNumbeType:'',sEA:'',STN:'',RPO:'',Ruralroute:''};    
 $scope.imm={done:false};
 $scope.poCode=""; 
 $scope.$watchCollection("[dem.health,dem.fname]", function(newValue, oldValue) {
@@ -66,56 +67,126 @@ connect.give(25);
 $scope.imm.done= !$scope.imm.done;    
 }
 
+$scope.dateOptions = {
+formatYear: 'yy',
+startingDay:1    
+};
+
+$scope.format='yyyy-MMMM-dd';    
 // yellow card input
    var i= 0;
                 $scope.vaccList = vaccineNames;
                 $scope.diseaseCol = diseaseCol;
- 
+    $scope.hist=[{date:''}];
+    $scope.historical = [{agent:''}];
+ $scope.agents=['MMR']
                 $scope.diseases= [{'entry':[
-        {'name': 'Diphtheria' },
-        { 'name':'Tetanus'},
-        { 'name':'Pertussis' },
-        {'name': 'Poliomyelitis'},
-        {'name':'Invasive haemophilus influenzae disease' },
-        {'name':'Pneumococcal disease' },
-                                { 'name':'Rotavirus enteritis'},
-        {'name':'Measles'},
-        {'name':'Mumps' },
-        {'name':'Rubella' },
-        {'name':'Varicella' },
-                                {'name':'Meningococcal disease' },
-        {'name':'Type B viral hepatitis' },
-        {'name':'Human papilloma virus infection' },
-        {'name':'Influenza' },
-        {'name':'Type A viral hepatitis' }
+        {'name': 'Diphtheria',
+        'checked' : false},
+        { 'name':'Tetanus',
+        'checked' : false},
+        { 'name':'Pertussis',
+        'checked' : false },
+        {'name': 'Poliomyelitis',
+        'checked' : false},
+      {'name': 'Poliomyelitis(IPV)',
+        'checked' : false},
+        {'name': 'Poliomyelitis(OPV)',
+        'checked' : false},
+        {'name':'Invasive haemophilus influenzae disease',
+        'checked' : false},
+        {'name':'Pneumococcal disease',
+        'checked' : false },
+        {'name':'Pneumo Conjugate disease',
+        'checked' : false },
+        {'name':'Pneumo Poly',
+        'checked' : false },
+        { 'name':'Rotavirus enteritis',
+        'checked' : false},
+        {'name':'Measles',
+        'checked' : false},
+        {'name':'Mumps',
+        'checked' : false },
+        {'name':'Rubella',
+        'checked' : false },
+        {'name':'Varicella',
+        'checked' : false },
+        {'name':'Meningococcal disease',
+        'checked' : false },
+        {'name':'Men-Conjugate C',
+        'checked' : false },
+        {'name':'Men-Conjugate-ACYX',
+        'checked' : false },
+        {'name':'Type B viral hepatitis',
+        'checked' : false},
+        {'name':'Human papilloma virus infection',
+        'checked' : false},
+        {'name':'Influenza',
+        'checked' : false},
+        {'name':'Type A viral hepatitis',
+        'checked' : false}
     ]}];
                
  
  
-                $scope.records=[{'Date':'dsfad','Vaccine':'','diseaseCol':$scope.diseases[0],'OtherDetails':''}];
+                $scope.records=[{'Date':$scope.hist[0],'Vaccine':$scope.historical[0],'diseaseCol':$scope.diseases[0],'OtherDetails':''}];
                 $scope.add= function(){
                
                 var n= 'next'+i;
-                var p= {'entry':[{'name': 'Diphtheria' },
-        { 'name':'Tetanus'},
-        { 'name':'Pertussis' },
-        {'name': 'Poliomyelitis'},
-        {'name':'Invasive haemophilus influenzae disease' },
-        {'name':'Pneumococcal disease' },
-                                { 'name':'Rotavirus enteritis'},
-        {'name':'Measles'},
-        {'name':'Mumps' },
-        {'name':'Rubella' },
-        {'name':'Varicella' },
-                                {'name':'Meningococcal disease' },
-        {'name':'Type B viral hepatitis' },
-        {'name':'Human papilloma virus infection' },
-        {'name':'Influenza' },
-        {'name':'Type A viral hepatitis' }]}
+                var p= {'entry':[
+        {'name': 'Diphtheria',
+        'checked' : false},
+        { 'name':'Tetanus',
+        'checked' : false},
+        { 'name':'Pertussis',
+        'checked' : false },
+        {'name': 'Poliomyelitis',
+        'checked' : false},
+      {'name': 'Poliomyelitis(IPV)',
+        'checked' : false},
+        {'name': 'Poliomyelitis(OPV)',
+        'checked' : false},
+        {'name':'Invasive haemophilus influenzae disease',
+        'checked' : false},
+        {'name':'Pneumococcal disease',
+        'checked' : false },
+        {'name':'Pneumo Conjugate disease',
+        'checked' : false },
+        {'name':'Pneumo Poly',
+        'checked' : false },
+        { 'name':'Rotavirus enteritis',
+        'checked' : false},
+        {'name':'Measles',
+        'checked' : false},
+        {'name':'Mumps',
+        'checked' : false },
+        {'name':'Rubella',
+        'checked' : false },
+        {'name':'Varicella',
+        'checked' : false },
+        {'name':'Meningococcal disease',
+        'checked' : false },
+        {'name':'Men-Conjugate C',
+        'checked' : false },
+        {'name':'Men-Conjugate-ACYX',
+        'checked' : false },
+        {'name':'Type B viral hepatitis',
+        'checked' : false},
+        {'name':'Human papilloma virus infection',
+        'checked' : false},
+        {'name':'Influenza',
+        'checked' : false},
+        {'name':'Type A viral hepatitis',
+        'checked' : false}
+                                 ]}
+                
+                
+                $scope.historical.push({'agent':''});
+                    $scope.hist.push({'date':''})
                                 $scope.diseases.push(p);
-                $scope.records.push({'Date':'dsfad','Vaccine':'','diseaseCol':$scope.diseases[$scope.diseases.length],'OtherDetails':'','id':''+i});
+   var length = $scope.diseases.length - 1;             $scope.records.push({'Date':$scope.hist[length],'Vaccine':$scope.historical[length],'diseaseCol':$scope.diseases[length],'OtherDetails':'','id':''+i});
 i=i+1;
- 
+ console.log(length);
                 }
  
 $scope.delete=function(id){
@@ -130,10 +201,96 @@ $scope.delete=function(id){
                                                 alert( "Something gone wrong" );
                                 }
                                 $scope.records.splice( index, 1 );
-        $scope.diseases.splice( index, 1);                    
+        $scope.diseases.splice( index, 1);
+    $scope.hist.splice(index,1);
+    $scope.historical.splice(index,1);
                 };
-                
+$scope.checkdis=function(id)
+{
+     var index = -1;                  
+                                for( var i = 0; i < $scope.records.length; i++ ) {
+                                                if( $scope.records[i].id === id) {
+                                                                index = i;
+                                                                break;
+                                                }
+                                }
+    console.log($scope.records[index].Vaccine);
+if($scope.records[index].Vaccine.agent =='MMR')
+{   $scope.records[index].diseaseCol.entry[0].checked = false;
+    $scope.records[index].diseaseCol.entry[1].checked = false;
+    $scope.records[index].diseaseCol.entry[2].checked = false;
+    $scope.records[index].diseaseCol.entry[3].checked = false;
+    $scope.records[index].diseaseCol.entry[4].checked = false;
+    $scope.records[index].diseaseCol.entry[5].checked = false;
+    $scope.records[index].diseaseCol.entry[6].checked = false;
+    $scope.records[index].diseaseCol.entry[7].checked = false;
+    $scope.records[index].diseaseCol.entry[8].checked = false;
+    $scope.records[index].diseaseCol.entry[9].checked = false;
+    $scope.records[index].diseaseCol.entry[10].checked = false;
+    $scope.records[index].diseaseCol.entry[11].checked = true;
+    $scope.records[index].diseaseCol.entry[12].checked = true;
+    $scope.records[index].diseaseCol.entry[13].checked = true;
+    $scope.records[index].diseaseCol.entry[14].checked = false;
+    $scope.records[index].diseaseCol.entry[15].checked = false;
+    $scope.records[index].diseaseCol.entry[16].checked = false;
+    $scope.records[index].diseaseCol.entry[17].checked = false;
+    $scope.records[index].diseaseCol.entry[18].checked = false;
+    $scope.records[index].diseaseCol.entry[19].checked = false;
+    $scope.records[index].diseaseCol.entry[20].checked = false;
+    $scope.records[index].diseaseCol.entry[21].checked = false;
 
+}
+else{
+console.log("error");
+}
+};
+$scope.checkvac= function(id)
+    {
+   
+      var index = -1;                  
+                                for( var i = 0; i < $scope.records.length; i++ ) {
+                                                if( $scope.records[i].id === id) {
+                                                                index = i;
+                                                                break;
+                                                }
+                                }
+        for (var j = 0; j< 22; j++){
+            
+            if (j == 11){
+                console.log(j)
+             continue;
+             }
+            else if (j == 12){
+                console.log(j)
+             continue;
+             }
+             else if (j == 13){
+                console.log(j)
+             continue;
+             }
+        else if ($scope.records[index].diseaseCol.entry[j].checked == true)
+        { console.log("Soorry!");
+          $scope.records[index].Vaccine.agent = '';
+            break;}
+        else if (j == 21)
+        {
+        if($scope.records[index].diseaseCol.entry[11].checked == true &&
+$scope.records[index].diseaseCol.entry[12].checked == true &&    
+$scope.records[index].diseaseCol.entry[13].checked == true){
+    {
+         console.log("two");
+    $scope.records[index].Vaccine.agent ='MMR'
+    }
+    }
+                  else{
+                      $scope.records[index].Vaccine.agent = '';
+        console.log("not MMR")
+        }
+        }
+        }
+}
+    
+    
   //modal   
 $scope.sucess="false";
 $scope.Agent;
@@ -147,22 +304,28 @@ $scope.info ={imm: '',provider: '',lotNumber: '',day:'',month:0,year:0};
     
     $scope.pload= function(){        
 $scope.payload=[{"data":"ICON","infoSrce":"email report",
-"patient":{"hcn":$scope.dem.health,"fName":$scope.dem.fname,"mName":$scope.dem.mname,"lName":$scope.dem.lname,"email":$scope.dem.sEA,"phoneNumberType":$scope.dem.SubmitterPhoneNumberType, "phone":$scope.dem.SubmitterPhoneNumber, "phoneNumberType2":$scope.dem.SubmitterPhoneNumberType2, "phone2":$scope.dem.SubmitterPhoneNumber2,"gender":$scope.dem.gender,"dob":$scope.dem.day,"uNo":$scope.dem.unumber,"sNo":$scope.dem.stnumber,"sName":$scope.dem.stname,"city":$scope.dem.city,"pCode":$scope.dem.poCode,
-"relation":{"relationship":$scope.dem.sr,"fName":$scope.dem.SubmitterPhoneNumberType,"lName":$scope.dem.SubmitterLN}},
-"immunization":[{"date":"","agent":"","lotNo":"","site":"","location":"","disease":""}],"recaptcha":document.getElementById("g-recaptcha-response").value
+"patient":{"hcn":$scope.dem.health,"fName":$scope.dem.fname,"mName":$scope.dem.mname,"lName":$scope.dem.lname,"email":$scope.dem.sEA,"phoneNumberType":$scope.dem.SubmitterPhoneNumberType, "phone":$scope.dem.SubmitterPhoneNumber, "phoneNumberType2":$scope.dem.SubmitterPhoneNumberType2, "phone2":$scope.dem.SubmitterPhoneNumber2,"gender":$scope.dem.gender,"dob":$scope.dem.day,"uNo":$scope.dem.unumber,"sNo":$scope.dem.stnumber,"addressType":$scope.dem.addressType,"Street Number":$scope.dem.stnum,"streetName":$scope.streetName,"streetType":$scope.dem.streetType,"streetDirection":$scope.dem.streetDirection,"sName":$scope.dem.stname,"city":$scope.dem.city,"pCode":$scope.dem.poCode,"STN":$scope.dem.STN,"RPO":$scope.dem.RPO,"RuralRoute":$scope.dem.Ruralroute,"Province":$scope.dem.prov,"Country":$scope.dem.country,
+"relation":{"relationship":$scope.dem.sr,"fName":$scope.dem.SubmitterFN,"lName":$scope.dem.SubmitterLN}},
+"immunization":[{"date":"","agent":"","snowmed":"","site":"","location":"","disease":""}],"recaptcha":document.getElementById("g-recaptcha-response").value
 }];
-  for(var h = 0; h <$scope.sums.length; h++)
-  { var a=$scope.sums[h].Agent;
-    var b=$scope.sums[h].Date;
-    var c=$scope.sums[h].Provider;
-    var d=$scope.sums[h].LotNumber;
-    $scope.payload[0].immunization[h]={"date":b,"agent":a,"lotNo":d,"provider":c, "disease":""};
+  for(var h = 0; h <$scope.records.length; h++)
+  { var a=$scope.records[h].Vaccine.agent;
+    var b=$scope.records[h].Date.date;
+   b=b.toISOString().substring(0,10);
+    var c=$scope.records[h].diseaseCol.entry;
+   var z;
+    if(a =='MMR')
+    {
+    z=61153008
+    }
+    $scope.payload[0].immunization[h]={"date":b,"agent":a, "disease":c,"snowmed":z};
   }
   console.log($scope.payload);
   document.getElementById("json").innerHTML = "Successfully loaded!"
 alert("Has loaded"+$scope.payload.length);
-        $http.post("http://captchaserver.mybluemix.net/captcha/",{obj:$scope.payload}).success(function(data,headers,config){}).
+        $http.post("http://ihrsubmit.mybluemix.net/submit/icon/demo",{obj:$scope.payload}).success(function(data,headers,config){}).
            error(function(data,headers,config){});
+        connect.give(100);
 
     };
 function send($scope){
@@ -212,6 +375,15 @@ $scope.opened="false"
 
 
 });
+app.directive('datepickerPopup', function(){
+return{
+restrict: 'EAC',
+require:  'ngModel',
+link: function(scope,element,attr,controller){
+controller.$formatters.shift();
+}    
+}
+});
 
 // Please note that $modalInstance represents a modal window (instance) dependency.
 // It is not the same as the $modal service used above.
@@ -247,6 +419,7 @@ $scope.opened = true;
     $modalInstance.dismiss('cancel');
   };
 });
+
 
 
  
